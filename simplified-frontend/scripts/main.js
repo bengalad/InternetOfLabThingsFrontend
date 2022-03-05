545b2fc5edc159dd46b020331c8a778d89d82453
bengalad <bengaladipo@hotmail.com>
2022-03-05 19:39:35 +0000

    adding comments

--- simplified-frontend/scripts/main.js
+++ simplified-frontend/scripts/main.js
@@ -1,20 +1,4 @@
-/*window.onload = function Tester() {
-    alert("Hi")
-    var name = prompt("What is your name?")
-    alert("Hey there, "+name)
-}
-*/
-
-//for measuremnet buttons
-const btns = document.querySelectorAll(".btn");
-
-btns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-    btn.classList.toggle("active");
-    });
-});
-
-//for the menu tab buttons
+//for the menu tab buttons - allows you swtich between stirrers
 const tabBtns = document.querySelectorAll(".tabBtn");
 
 tabBtns.forEach((tabBtn) => {
@@ -36,50 +20,30 @@ function removeClasses(target) {
     });
 }
 
-//const checks = document.getElementsByClassName("checkmark");
-//array = console.log(checks);
 
-//let displaySetting = document.getElementsByClassName("tabBtn").style.display;
+//for measuremnet buttons - allows you to select and unselect
+const btns = document.querySelectorAll(".btn");
 
-
-
-/*
-function displaybutton(){
-    let x = document.getElementsByClassName("one").style.display;
-    console.log("testing");
-    x2 = console.log(x);
-    x = "block"
-    if (x.display == 'none') {
-        x.display = 'block';
-        console.log("works");
-      } else {
-        x.display = 'none';
-        console.log("does something")
-      }
-}
-*/
-
-//let idList = ";"
-//document.getElementById('textbox1').value = idList;
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+    btn.classList.toggle("active");
+    });
+});
 
 
 //for enabling/disabling tab menu buttons using checkbox
 function EnableDisableButton(cb, id) {
 
   if (cb.checked == 1) {
-    
-    /*idList = idList + id + ";"
-    console.log(idList);*/
-    //document.getElementById('textbox1').value = idList;
     document.getElementById(id).disabled = false;
   }
   else{
     document.getElementById(id).disabled = true;
     document.getElementById(id).classList.remove("active"); 
   }
-
 }
 
+//function to send stirrer configurations 
 function connect(){
 
 }
